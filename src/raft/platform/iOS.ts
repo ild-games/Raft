f85c6efb1765c0323ecf9b9c081baead05c392ed
tlein <tlein@radford.edu>
2017-08-19 17:26:45 -0500

    add toolchain for ios

--- src/raft/platform/iOS.ts
+++ src/raft/platform/iOS.ts
@@ -1,12 +1,13 @@
 import {Build, Platform, Architecture} from '../core/build-config';
 import {Flag, RAFT_FLAGS} from '../core/flags';
+import {raftCMakeDir} from '../core/cmake';
 
 export class iOSPlatform extends Platform {
     name : string = "iOS";
 
     getArchitectures() : iOSArchitecture[] {
         return [
-
+            "armeabi"
         ].map(name => new iOSArchitecture(name));
     }
 }
@@ -20,7 +21,16 @@ class iOSArchitecture extends Architecture {
         return [
             {name : RAFT_FLAGS.IS_DESKTOP, value: RAFT_FLAGS.FALSE},
             {name : RAFT_FLAGS.IS_ANDROID, value: RAFT_FLAGS.FALSE},
-            {name : RAFT_FLAGS.IS_IOS, value: RAFT_FLAGS.TRUE}
+            {name : RAFT_FLAGS.IS_IOS, value: RAFT_FLAGS.TRUE},
+            {name : RAFT_FLAGS.CMAKE_TOOLCHAIN, value: raftiOSToolchainFile().toString()},
         ]
     }
+
+    getCMakeGeneratorTarget() : string | null {
+        return "Xcode";
+    }
+}
+
+function raftiOSToolchainFile() {
+    return raftCMakeDir().append("toolchains","iOS","iOS.toolchain.cmake");
 }
